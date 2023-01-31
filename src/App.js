4c45266b7e0d8869f577d6b741b0121204bb3a07
Trygve Horn <110484917+Thorn5@users.noreply.github.com>
2023-01-31 23:18:29 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,55 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import "./styles.css";
+import React from "react";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <Fetch1 />
+    <div className="App">
+      <h1>Main Branch, climb somewhere else!</h1>
     </div>
-
   );
 };
 
 export default App;
-
-const Fetch1 = () => {
-
-  // Declare state variables
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  // Fetch data when the component is mounted
-  useEffect(() => {
-    setLoading(true);
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(json => {
-        setData(json);
-        setLoading(false);
-      })
-      .catch(error => {
-        setError(error);
-        setLoading(false);
-      });
-  }, []);
-
-  // Render data or loading/error message
-  return (
-    <div className="App">
-      {loading ? (
-        <div>Loading...</div>
-      ) : error ? (
-        <div>An error occurred: {error.message}</div>
-      ) : data ? (
-        <ul>
-          {data.map(item => (
-            <li key={item.id}>{item.title}</li>
-          ))}
-        </ul>
-      ) : (
-        <div>No data</div>
-      )}
-    </div>
-  );
-}
