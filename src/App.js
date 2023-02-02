95a51c4266ca32ea7064943aa8eec13c6cafcdeb
Trygve Horn <trygvehorn@gmail.com>
2023-02-02 22:31:21 +0100

    ss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
-import "./App.css";
+//App.js 
+import useDataFetch from "./CustomHook/useDataFetch";
 
-const App = () => {
+const urlBase = 'https://api.genderize.io/';
+const queryParams = '?name=';
+const queryString = 'Trygve';
+
+function App() {
+  const { data, loading, error } = useDataFetch(urlBase, queryParams, queryString);
+
+  if (loading) {
+    return <p>Loading ...</p>;
+  }
+
+  if (error) {
+    return <p>Error</p>;
+  }
+
   return (
     <div className="App">
-      <h1>Main Branch, climb somewhere else!</h1>
+      {data && <p>Data fetched successfully.</p>}
     </div>
   );
-};
-
+}
 export default App;
---
+++ src/CustomHook/useDataFetch.js
@@ -0,0 +1,34 @@
+//./CustomHook/useDataFetch.js
+
+import { useState, useEffect } from "react";
+
+// const urlBase = 'https://api.example.com/';
+// const queryParams = '?search=';
+// const queryString = 'test';
+
+const useDataFetch = (urlBase, queryParams, queryString) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setLoading(true);
+        try {
+            fetch(urlBase+queryParams+queryString)
+                .then(response => response.json())
+                .then(jsonResponse => {
+                    setData(JSON.parse(jsonResponse));
+                    // setLoading(false);
+                })
+        } catch (err) {
+            setError(err);
+            // setLoading(false);
+        } finally {
+            setLoading(false);
+        }
+        console.log({ data, loading, error });
+    }, [data, loading, error]);
+    
+    return { data, loading, error };
+};
+export default useDataFetch;
